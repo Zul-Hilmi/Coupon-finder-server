4f04c1df7c62cfcde065498e846b2c3f221332d6
Zul Hilmi <2021101271@student.uitm.edu.my>
2022-12-03 20:26:14 +0800

    clientError to ClientError

--- dist/middlewares/errorHandler.js
+++ dist/middlewares/errorHandler.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-const clientError_1 = __importDefault(require("../config/clientError"));
+const clientError_1 = __importDefault(require("../config/ClientError"));
 const errorHandler = (err, req, res, next) => {
     console.log(err);
     if (err instanceof mongoose_1.default.Error.ValidationError) { //native mongoose error
